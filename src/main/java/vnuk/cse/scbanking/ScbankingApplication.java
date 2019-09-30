57e9efef27d912d159d7f5a50e153083dc444ec8
tkppro <tkpproo@gmail.com>
2019-09-30 19:31:26 +0700

    init project

---
+++ src/main/java/vnuk/cse/scbanking/ScbankingApplication.java
@@ -0,0 +1,13 @@
+package vnuk.cse.scbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScbankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScbankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vnuk/cse/scbanking/ScbankingApplicationTests.java
@@ -0,0 +1,16 @@
+package vnuk.cse.scbanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScbankingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
