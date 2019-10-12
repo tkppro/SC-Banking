a520ab9f0f5b42f009db96aee2d8aa013695ac7f
Eric <anh.tran160201@vnuk.edu.vn>
2019-10-13 06:38:13 +0700

    Fix entity

--- src/main/java/vnuk/cse/scbanking/entity/Bill.java
+++ src/main/java/vnuk/cse/scbanking/entity/Bill.java
@@ -3,6 +3,7 @@ package vnuk.cse.scbanking.entity;
 import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @Entity
@@ -12,11 +13,16 @@ public class Bill implements Serializable {
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    public int id;
+    private int id;
 
     @Column(name = "name")
-    public String name;
+    private String name;
 
     @Column(name = "created_at")
-    public String createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+//    @OneToMany
+//    @JoinColumn(name = "id", nullable = false)
+//    private Payment payment;
 }
