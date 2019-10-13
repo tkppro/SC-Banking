99e5199a98e352a1a3f4b7fa37909a661a45057e
Hoang Anh Vu Tran <anh.tran160201@vnuk.edu.vn>
2019-10-13 04:10:57 +0000

    Eric user entity service repo

---
+++ src/main/java/vnuk/cse/scbanking/service/UserService.java
@@ -0,0 +1,19 @@
+package vnuk.cse.scbanking.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vnuk.cse.scbanking.entity.User;
+import vnuk.cse.scbanking.repositories.UserRepository;
+import java.util.List;
+
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+}
