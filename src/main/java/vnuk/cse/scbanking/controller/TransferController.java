cad19934cd008075290bbd812d14b1ee538ca55d
tkppro <tkpproo@gmail.com>
2019-10-20 17:07:41 +0700

    fix route and change controller for transfer

--- src/main/java/vnuk/cse/scbanking/controller/TransferController.java
+++ src/main/java/vnuk/cse/scbanking/controller/TransferController.java
@@ -3,6 +3,8 @@ package vnuk.cse.scbanking.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import vnuk.cse.scbanking.entity.Wallet;
 import vnuk.cse.scbanking.service.WalletService;
@@ -19,6 +21,24 @@ public class TransferController {
         ModelAndView modelAndView = new ModelAndView("pages/transfer");
         List<Wallet> wallets = walletService.findAll();
         modelAndView.addObject("wallets",wallets);
+        return modelAndView;
+    }
+
+    @PostMapping("/transfer")
+    public ModelAndView transfer(@RequestParam("from-wallet") int fromWalletId,
+                                 @RequestParam("to-wallet") int toWalletId,
+                                 @RequestParam("amount") double amount)
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        if(walletService.transfer(fromWalletId, toWalletId, amount))
+        {
+            modelAndView.setViewName("redirect:/dashboard");
+
+            return modelAndView;
+        }
+
+        modelAndView.setViewName("redirect:/transfer");
+
         return modelAndView;
     }
 }
